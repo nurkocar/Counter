d9721b9866d016c20f072800669fec8b05730c0a
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-09-30 23:14:17 +0000

    create counter needs update

---
+++ app.js
@@ -0,0 +1,41 @@
+const headingElement = document.getElementsByClassName('heading');
+const value = document.querySelector('#value');
+const btnElements = document.querySelectorAll('.btn');
+
+let count = 0;
+//parseFloat(getComputedStyle(e.currentTarget).fontSize) + 1;
+let newFontSize = parseFloat(getComputedStyle(h1).fontSize);
+
+btnElements.forEach(btn =>{
+    btn.addEventListener('click', function(){
+
+        
+
+        if (btn.className.includes('decrease')){
+            count--;
+            // newFontSize--;            
+        }
+        else if (btn.className.includes('increase')){
+            count++;
+            // newFontSize++; 
+        }
+        else{
+            count = 0;
+            // newFontSize = '32px';
+        }
+
+        if (count > 0){
+            value.style.color = 'green';
+        }
+        else if (count < 0){
+            value.style.color = 'red';
+        }
+        else{
+            value.style.color = 'black';
+        }
+
+        value.textContent = count;
+        // h1Element.style.fontSize = `${newFontSize}px`;
+
+    });
+})
